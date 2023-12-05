a218230182ab877ddb859f0ad1a299e734744be5
Mehad-242.com <mehadboosh@gmail.com>
2023-12-04 18:58:47 -0800

    a

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <python.h>
+
+/**
+ * print_python_list_info - prints basic info about python lists
+ * @p: A pyObject list
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, i;
+	PyObject *obj;
+
+	size = py_SIZE(p);
+	alloc = ((pyListObject *)p)->allocated;
+
+	printf("[*] size of the python list = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+		obj = pyList_GetItem(p, i);
+		printf("%s\n", py_TYPE(obj)->tp_name);
+	}
+}
