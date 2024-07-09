842934da382eaf5c864e07896d0c2f6969a4d5a1
Mehad-242 <mehadboosh@gmail.com>
2024-07-09 08:54:01 -0700

    m

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL and displays the      size of the body of the response
-curl -sI $1 | grep "content-length" | cut -d " " -f2
+
+# Replest with your desired URL
+# SEND A GET request and calculate the response size
+curl -Is "$1" | grep -w "content-length" | cut -f2 -d' '
