3edb8f2da6a9162f223e2a93f133623482ab3e82
Mehad-242 <mehadboosh@gmail.com>
2024-07-09 05:34:07 -0700

    m

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 # takes in a URL, sends a request to that URL and displays the      size of the body of the response
 curl -sI $1 | grep "content-length" | cut -d " " -f2
