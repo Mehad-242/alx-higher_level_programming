79981d7f51d680ac652043f0cdf58624d1ba223f
Mehad-242 <mehadboosh@gmail.com>
2024-07-09 05:26:06 -0700

    m

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/python3
-# takes in a URL, sends a GET request to the URL
-curl -sX GIT $1 -L
+#!/user/bin/python3
+# check if URL aegument is provided
+if [ $# -ne 1 ];then
+	echo "Usage: $0 <URL>"
+	exit 1
+fi
+# sends a GET request using curl
+curl -sX "$1" 
